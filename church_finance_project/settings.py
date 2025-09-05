d5a5a5261d0582f6253b5c9da66b6a91a1b8a03d
Gregory Grant <grant8827@yahoo.com>
2025-09-05 14:46:46 -0400

    fix error 1

--- church_finance_project/settings.py
+++ church_finance_project/settings.py
@@ -89,10 +89,25 @@ WSGI_APPLICATION = "church_finance_project.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 # Default to SQLite for local development
+#DATABASES = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.sqlite3',
+   #     'NAME': BASE_DIR / 'db.sqlite3',
+    #}
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME', 'railway'),
+        'USER': os.getenv('DB_USER', 'root'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'dmqXKCvymXJrmwlAUTYtODBvRAuUxETT'),
+        'HOST': os.getenv('DB_HOST', 'centerbeam.proxy.rlwy.net'),
+        'PORT': os.getenv('DB_PORT', '21213'),
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        }
     }
 }
 
