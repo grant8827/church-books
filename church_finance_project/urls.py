dd39c4dc5f38700a2078fa05e76a0f1967af0f1f
Gregory Grant <grant8827@yahoo.com>
2026-02-18 16:54:58 -0500

    Fix media serving, bank transfer payment,  pricing, simplified admin, trial banner logic

--- church_finance_project/urls.py
+++ church_finance_project/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.views.generic import TemplateView, RedirectView # For a simple home page
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 from church_finances.admin_site import church_admin_site
 from church_finances.views_health import health_check
 from church_finances.railway_health import railway_health_check
@@ -48,5 +49,7 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-# Always serve media files (church logos etc.) - Django handles this directly
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Always serve media files (church logos etc.) regardless of DEBUG mode
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
